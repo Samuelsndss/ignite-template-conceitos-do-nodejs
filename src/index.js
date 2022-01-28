ec55a8c08fc8d1db6961650256804365e6035512
Samuelsndss <69823253+Samuelsndss@users.noreply.github.com>
2022-01-27 21:23:58 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,55 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+
+  const {username} = request.headers; 
+
+  const userExist = users.find(userExist => 
+    userExist.username == username
+    );
+
+    if(!userExist) {
+      return response.status(400).json({error: "Já Existe Mensagem de erro!! "});
+    }
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const {name, username} = request.body;
+
+  const userAlreadyExist = users.some(
+    (userExist) => userExist.username === username
+  );
+
+  if(userAlreadyExist) {
+    return response.status(400).json({error: "Essa Conta Existe"});
+  }
+
+  users.push({
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  });
+
+  return response.status(201).json(users);
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get('/todos', (request, response) => {
+  const { users } = request;
+
+  return response.json(users);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
